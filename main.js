76fe6fb3b5f9ff139905955f9f6ebcde67dfca90
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-11-28 18:22:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+noseX = 0 ;
+noseY = 0 ;
+
 function preload() {
-    
+    clown_nose = loadImage('https://i.postimg.cc/hj0PvP6z/clown-nose.png') ;
 }
 
 function setup() {
@@ -20,13 +23,16 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results) ;
-        console.log("nose x :" + results[0].pose.nose.x) ;
-        console.log("nose y :" + results[0].pose.nose.y) ;
+        noseX = results[0].pose.nose.x ;
+        noseY = results[0].pose.nose.y ;
+        console.log("Nose X = " + noseX) ;
+        console.log("Nose Y = " + noseY) ;
     }
 }
 
 function draw() {
     image(video , 0 , 0 , 300 , 300) ;
+    image(clown_nose , noseX , noseY , 30 , 30) ;
 }
 
 function take_snapshot() {
